d7b227b81798ead5e439cb40ca17f4deea87d355
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-10-05 20:45:20 +0500

    basic stripe payment checkout

---
+++ gatsby-ecommerce-1-stripe-checkout/src/pages/payment-error.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+// markup
+const PaymentError = () => {
+
+  return (
+    <div style={{width: "100vw", height: "100vh", justifyContent:"center", display: "flex", alignItems:"center"}}>
+        
+        <h1 style={{color: "red"}}>Payment Has Been Denied</h1>
+
+    </div>
+  )
+}
+
+export default PaymentError
---
+++ gatsby-ecommerce-1-stripe-checkout/src/pages/payment-success.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+// markup
+const PaymentSuccess = () => {
+
+  return (
+    <div style={{width: "100vw", height: "100vh", justifyContent:"center", display: "flex", alignItems:"center"}}>
+        
+        <h1 style={{color: "green"}}>Payment Has Been Successful</h1>
+
+    </div>
+  )
+}
+
+export default PaymentSuccess
